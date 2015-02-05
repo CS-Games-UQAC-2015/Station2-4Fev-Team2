176c7ce319fd84389c092e678adb4ffd6468017d
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-02-05 03:01:17 +0000

    fock it

--- RepeaterMain.py
+++ RepeaterMain.py
@@ -1,14 +1,39 @@
 filename = "A-small-practice.in"
-with open(filename) as txt:
-    casesAmount = int(txt.readline())
 
-    for i in range(casesAmount):
-        inputAmount = int(txt.readline())
-        str = ['']*inputAmount
+def resolve(lines):
+    # Sort lines by length order (smallest first)
+    lines.sort(key=len)
+    
+    shortest = 99999999
+    
+    # Get unique characters in all strings
+    for i in range(len(lines)):
+        line = lines[i]
         
-        for j in range(inputAmount):
-            line = txt.readline()
-            str[j] = line
-            #for k in range(len(line))
+        if i == 0:
+            continue;
             
-        print('Case #',i,': ')
+        # If the unique characters of this line aren't the same as the last line, it's impossibru
+        if (set(lines[i-1]) != set(line)):
+            return "Fegla Won"
+    
+    return shortest
+
+def main():
+    with open(filename) as txt:
+        casesAmount = int(txt.readline())
+    
+        for i in range(casesAmount):
+            inputAmount = int(txt.readline())
+            lines = []
+            
+            # Get all lines
+            for j in range(inputAmount):
+                line = txt.readline().rstrip('\n')
+                lines.append(line)
+                
+            # Solve
+            print("Case #" + str(i) + ": " + str(resolve(lines)))
+            
+if __name__ == "__main__":
+    main()
